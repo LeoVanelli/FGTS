ae6a353a59634a2d7d6550cad7ba8818d324fee6
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-26 23:44:46 -0300

    Adiciona atividade FGTS

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Objetivo: Calcule a parcela do FGTS sobre o salário de um funcionário (8%).
+
+            Console.Write("Digite o salário do funcionário: R$ ");
+            float salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nFGTS: R$ {(salario*8)/100}");
+        }
+    }
+}
